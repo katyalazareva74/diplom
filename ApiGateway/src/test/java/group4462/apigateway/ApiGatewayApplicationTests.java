b530b370dddb243f3bbc6f826aa1e759c705096c
Ekaterina Lazareva <lazarevakatya@mail.ru>
2024-03-24 22:42:53 +0300

    First commit

---
+++ ApiGateway/src/test/java/group4462/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package group4462.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
